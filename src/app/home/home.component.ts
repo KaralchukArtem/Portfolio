812978071e01c6adc209c06e518dd4aa2c40dc99
BlackOuT <artuom444@inbox.ru>
2020-02-11 02:11:08 +0300

    feat: New theam

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Subscriber } from 'rxjs';
+
 import * as M from '../../assets/materialize/js/materialize.min.js';
 
 @Component({
@@ -14,5 +17,9 @@ export class HomeComponent implements OnInit {
     var carousel = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(carousel, this.options);
   }
+
+  date = new Observable<string>((observer: Subscriber<string>) => {
+    setInterval(() => observer.next(new Date().toString()), 1000);
+  });
 
 }
